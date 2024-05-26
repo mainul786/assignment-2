9602672201b08099510ce9ee1508620bcf0208ab
mainul96 <110577655+mainul96@users.noreply.github.com>
2024-05-26 23:10:00 +0530

    product interface declare

---
+++ src/modules/products/products.interface.ts
@@ -0,0 +1,19 @@
+export type TProductVariant = {
+  type: string;
+  value: string;
+};
+
+export type TProductInvetory = {
+  quantity: number;
+  inStock: boolean;
+};
+
+export type TProducts = {
+  name: string;
+  description: string;
+  price: number;
+  category: string;
+  tags: string[];
+  variants: TProductVariant[];
+  inventory: TProductInvetory;
+};
